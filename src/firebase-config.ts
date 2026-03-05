e3c012b7703530b9c778871c3ba5f69ef9d8fb3b
Caliderumba <caliderumba@gmail.com>
2026-03-05 14:22:01 -0500

    Set up Firebase configuration and messaging
    Initialize Firebase app and messaging service with configuration.

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,1 +1,15 @@
+ import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
 
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID",
+  measurementId: "G-RRYHESGWKF" // Tu ID de Analytics que ya tenemos
+};
+
+const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
